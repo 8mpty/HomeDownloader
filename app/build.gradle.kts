e9da8934a87205c1042f2bca44a63f880a544552
8mpty <mohdkhairin62@gmail.com>
2024-04-27 19:36:06 +0800

    added internet & storage perms, added libraries (okhttp3 & gson)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,14 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // Custom/added implementations
+    val okhttp3Ver = "4.12.0"
+    implementation("com.squareup.okhttp3:okhttp:${okhttp3Ver}")
+
+    val gsonVer = "2.10.1"
+    implementation("com.google.code.gson:gson:${gsonVer}")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
