d94d10e21e57d120a53d8df2c9f78c8964fe02cf
8mpty <mohdkhairin62@gmail.com>
2024-04-27 20:34:04 +0800

    added recyclerview and custom itemrows

--- app/src/main/java/com/empty/homedownloader/MainActivity.kt
+++ app/src/main/java/com/empty/homedownloader/MainActivity.kt
@@ -2,10 +2,17 @@ package com.empty.homedownloader
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rv = findViewById<RecyclerView>(R.id.mainRV)
+
+        rv.layoutManager = LinearLayoutManager(this)
+        rv.adapter = RVAdapter()
     }
 }
